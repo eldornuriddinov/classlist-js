747447d6237b58cacbbbca5f795283889c68302b
eldornuriddinov <nuriddinoveldor133@gmail.com>
2024-08-17 16:51:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+
+
+
+// const color = prompt('Choose color: red, blue or green')
+
+
+
+// const box = document.querySelector('.box')
+
+// console.log(box);
+
+
+
+
+// if (color === 'red'){
+//     box.classList.add('red')
+// } else if(color === 'green'){
+//     box.classList.add('green')
+// } else if(color === 'blue'){
+//     box.classList.add('blue')
+// } else{
+//     alert('You entered invalid text.')
+// }
+
+
+
+
+
+
+
+
+
+// const title = document.querySelector('h1')
+
+// const newText = prompt('Enter new text for a title:')
+
+// if(newText){
+//     title.textContent = newText
+// } else {
+//     alert('You entered nothing')
+// }
+
+
+
+
+
+
+
+
+// const name = document.querySelector('.box h1')
+// const surname = document.querySelector('.box h2')
+// const age = document.querySelector('.box h3')
+// const image = document.querySelector('.box img')
+
+
+// const firsName = prompt('Your name')
+// const lastName = prompt('Your surname')
+// const enterAge = prompt('Your age')
+// const enterImage = prompt('Your image')
+
+
+// name.textContent = firsName
+// surname.textContent = lastName  
+// age.textContent = enterAge
+// image.src = enterImage
